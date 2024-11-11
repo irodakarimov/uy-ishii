0a6c9a95618ae3edcb3f9d2759179e2c0c0227a9
irodakarimov <irodakarimova896@gmail.com>
2024-11-11 16:14:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = 5
+let b = 853290
+const sux = 87
+
+let userName = prompt('Ismingizni kiriting');
+let surName = prompt('Familiyangizni kiriting');
+let ageName = prompt('Yoshingizni kiriting');
+let favoritesubject = prompt('Yoqtirgan faningiz yozing');
+let livingdistrict = prompt('Yashayotgan tumaningiz kiriting');
+alert(`Malumotlaringizni kiritganingiz uchun tashakkur ${userName}`);
